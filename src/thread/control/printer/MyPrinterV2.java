189e8836e758d134d32c4b1b7951ff83b6b583a4
chrissykrissy <jj2174@nyu.edu>
2024-10-08 23:10:21 +0900

    Thread interrupt and yield Study

---
+++ src/thread/control/printer/MyPrinterV2.java
@@ -0,0 +1,54 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class MyPrinterV2 {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printThread = new Thread(printer, "printer");
+        printThread.start();
+        Scanner userInput = new Scanner(System.in);
+        while(true){
+            log("프린터할 문서를 입력하세요. 종료(q): ");
+            String input = userInput.nextLine();
+            if (input.equals("q")) {
+                printer.work = false; //while문을 체크하는 곳에서 빠져나온다.
+                printThread.interrupt(); //sleep()상태에서 빠져나온다.
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+    static class Printer implements Runnable{
+        volatile boolean work = true;
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while(work){
+                if (jobQueue.isEmpty()) {
+                    continue;
+                }
+                try {
+                    String job = jobQueue.poll();
+                    log("출력 시작: " + job + ", 대기문서: " + jobQueue);
+                    Thread.sleep(3000);
+                    log("출력 완료");
+                } catch (InterruptedException e) {
+                    log("인터럽트!");
+                    break;
+                }
+            }
+            log("프린터 완료");
+        }
+
+        public void addJob(String input){
+            jobQueue.add(input);
+        }
+    }
+}
